29cac16e52e790873f4da617f9c8ae5f16a273a1
azwri <aazwri@gmail.com>
2024-09-03 18:51:29 +0300

    add diveces

--- arduino/admin.py
+++ arduino/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import SensorData
+from .models import SensorData, Device
 
 @admin.register(SensorData)
 class SensorDataAdmin(admin.ModelAdmin):
     list_display = ('temperature', 'humidity', 'aqi', 'ppm', 'ppm')
     list_filter = ('temperature', 'humidity', 'aqi', 'ppm', 'ppm')
     search_fields = ('temperature', 'humidity', 'aqi', 'ppm', 'ppm')
+
+
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ('device_id', 'device_name', 'device_type')
+    list_filter = ('device_id', 'device_name', 'device_type')
+    search_fields = ('device_id', 'device_name', 'device_type')
