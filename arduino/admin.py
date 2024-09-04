e14a51acc9707b4389cd97f370e354c3c47e2017
azwri <aazwri@gmail.com>
2024-09-04 05:50:29 +0300

    add maps

--- arduino/admin.py
+++ arduino/admin.py
@@ -3,7 +3,7 @@ from .models import SensorData
 
 @admin.register(SensorData)
 class SensorDataAdmin(admin.ModelAdmin):
-    list_display = ('temperature', 'humidity', 'aq', 'ppm')
+    list_display = ('temperature', 'humidity', 'aqi', 'ppm')
     list_filter = ('temperature', 'humidity', 'aqi', 'ppm', )
     search_fields = ('temperature', 'humidity', 'aqi', 'ppm',)
 
